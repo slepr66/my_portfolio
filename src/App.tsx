05573d5627dc171f9e4c086cbb94e16cbc53351f
slepr66 <shinaniyash@gmail.com>
2025-03-23 15:03:06 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Card from "./components/card"
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/card.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+        <div className="w-[700px] h-[500px] bg-[#171717] rounded-3xl drop-shadow-md">
+
+        </div>
+    )
+}
